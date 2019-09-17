eb98a63d947dc473730c93af05633bebaf557bdf
TureeZhang <aa940724@outlook.com>
2019-09-17 14:12:01 +0800

    Update upload.ts

--- Angular/upload.ts
+++ Angular/upload.ts
@@ -1,1 +1,20 @@
+/* https://github.com/NG-ZORRO/ng-zorro-antd/blob/master/components/upload/nz-upload-btn.component.html */
 
+`
+<input type="file" #file (change)="onChange($event)"
+  [attr.accept]="options.accept"
+  [attr.directory]="options.directory ? 'directory': null"
+  [attr.webkitdirectory]="options.directory ? 'webkitdirectory': null"
+  [multiple]="options.multiple" style="display: none;">
+<ng-content></ng-content>
+`;
+
+@ViewChild('file', { static: false }) file: ElementRef;
+
+@HostListener('click')
+onClick(): void {
+  if (this.options.disabled || !this.options.openFileDialogOnClick) {
+    return;
+  }
+  (this.file.nativeElement as HTMLInputElement).click();
+}
