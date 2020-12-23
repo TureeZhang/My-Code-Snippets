d669272aca7833f0b327f2e907251411c430f616
TureeZhang <aa940724@outlook.com>
2020-12-23 17:20:35 +0800

    Update 返回可观察对象.ts

--- Angular/返回可观察对象.ts
+++ Angular/返回可观察对象.ts
@@ -26,3 +26,25 @@
 //视图上可以使用 |async 管道处理可观察对象
 
 <button *ngIf="pageStatus==0 && (isAdmin|async)" nz-button nzType="primary" (click)="edit()" style="margin-left:16px;"><i nz-icon type="edit"></i>编 辑</button>
+
+  
+isDebugPortDuplicated = (control: FormControl): Observable<ValidationErrors | null> => {
+  return new Observable((observer: Observer<ValidationErrors | null>) => {
+    if (this.timerForDebugPortDuplicated !== null) { //请求防抖
+      clearTimeout(this.timerForDebugPortDuplicated);
+    }
+    let debugPort: number = control.value;
+    this.isAsyncValidateDebugPort = true;
+    this.timerForDebugPortDuplicated = setTimeout(() => {
+      this.beflamApiProjectService.isDebugPortDuplicated(debugPort).subscribe(response => {
+        if (response === true) {
+          observer.next({ error: true, duplicated: true }); //必须返回 error:true 以标识此事件为校验错误
+        } else {
+          observer.next(null);
+        }
+        this.isAsyncValidateDebugPort = false;
+        observer.complete();
+      });
+    }, 1000);
+  });
+}
